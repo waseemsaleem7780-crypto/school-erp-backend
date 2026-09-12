754e76a50ea987085181f6fe201d47a822377ea3
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-09-12 16:08:19 +0500

    Use env variable for SECRET_KEY

--- utils/jwt_handler.py
+++ utils/jwt_handler.py
@@ -1,34 +1,23 @@
+import os
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 
-
-SECRET_KEY = "my_super_secret_key_123" 
+SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_key_123")
 ALGORITHM = "HS256"
 EXPIRY_MINUTES = 1440   # 24 hours
 
 
 def create_access_token(data: dict):
-
     copy_data = data.copy()
-
     expire = datetime.utcnow() + timedelta(minutes=EXPIRY_MINUTES)
-
-    copy_data.update({"exp":expire})
-
-    token = jwt.encode(copy_data,SECRET_KEY,algorithm=ALGORITHM)
-
+    copy_data.update({"exp": expire})
+    token = jwt.encode(copy_data, SECRET_KEY, algorithm=ALGORITHM)
     return token
 
 
-def decode_access_token(token : str):
+def decode_access_token(token: str):
     try:
-        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
-
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
-    
     except JWTError:
-
         return None
-
-
-    
