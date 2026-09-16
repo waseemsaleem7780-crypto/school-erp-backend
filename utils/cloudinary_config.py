60890484eec72867ff2c2bf882d0c05f04145d0b
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-09-16 14:33:26 +0500

    Fix cloudinary config and values

--- utils/cloudinary_config.py
+++ utils/cloudinary_config.py
@@ -3,7 +3,7 @@ import cloudinary.uploader
 import os
 
 cloudinary.config(
-    cloud_name=os.getenv("Root"),
+    cloud_name=os.getenv("goc0hcbp"),
     api_key=os.getenv("474794289596643"),
     api_secret=os.getenv("NKP5_-2a8L4GFMNJ_cxMQObNRtk"),
     secure=True
